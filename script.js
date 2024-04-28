ea5840c57cbc7b10b07dcca2fa470063752989e7
Renzios <61353378+Renzios@users.noreply.github.com.>
2024-04-28 18:54:26 +0800

    fix: edit createGrid and changeColor

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
-const container = document.querySelector(".container");
+createGrid(16);
+changeColor("black");
 
-let size = prompt("Size");
+function createGrid(size) {
+    const container = document.querySelector(".container");
 
-for (let i = 0; i < size; i++) {
-    const row = document.createElement("div");
-    row.classList.add("row");
-    container.appendChild(row);
-
-    for (let j = 0; j < size; j++) {   
-        const column = document.createElement("div");
-        column.classList.add("square");
-        row.appendChild(column);
+    for (let i = 0; i < size; i++) {
+        const row = document.createElement("div");
+        row.classList.add("row");
+        container.appendChild(row);
+    
+        for (let j = 0; j < size; j++) {   
+            const column = document.createElement("div");
+            column.classList.add("square");
+            row.appendChild(column);
+        }
     }
 }
 
-const squares = document.querySelectorAll(".square");
+function changeColor(color) {
+    const squares = document.querySelectorAll(".square");
 
-let color = prompt("Color");
-
-squares.forEach((square) => {
-    square.addEventListener("mouseenter", () => square.style.backgroundColor = color);
-});
+    squares.forEach((square) => {
+        square.addEventListener("mouseenter", () => square.style.backgroundColor = color);
+    });    
+}
